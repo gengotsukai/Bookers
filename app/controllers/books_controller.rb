1ddee44bcb300d0c8c4d091f5cb81890a3df5358
gengotsukai <benioni2005@yahoo.co.jp>
2020-02-29 21:50:42 +0900

    kyou wa owari

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,7 +18,13 @@ class BooksController < ApplicationController
   def update
       book = Book.find(params[:id])
       book.update(book_params)
-      redirect_to book_path(list.id)
+      redirect_to book_path(book.id)
+  end
+
+  def destroy
+      book = Book.find(params[:id]) #データ(レコード)を1件取得
+      book.destroy #データ（レコード）を削除
+      redirect_to books_path #List一覧画面へリダイレクト
   end
 
   def create
